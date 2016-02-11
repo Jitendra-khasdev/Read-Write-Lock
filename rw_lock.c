f37b5af8fde8a2141f8716cbe966083c0a484dd6
Jitendra Kumar Khasdev <jkhasdev@gmail.com>
2016-02-11 18:26:57 +0530

    Added read write lock support

--- rw_lock.c
+++ rw_lock.c
@@ -9,11 +9,8 @@
 #include <semaphore.h>
 #include "workqueue.h"
 
-
-int counter = 0;
-
-sem_t mutex;
-//int read_count = 0;
+sem_t			mutex;
+pthread_rwlock_t	rwlock;
 
 struct threadpool *tp = NULL;
 
@@ -24,84 +21,61 @@ int data = 5;
 
 /*
  * Reader function for reader thread
- *
  */
-
 int reader()
 {
-	/*sem_wait(&mutex);
-	read_count++;
-	if (read_count == 1)
-		sem_wait(&wrt);
-	sem_post(&mutex);
-
-	sleep(3);*/
-	printf("Reader Thread\n");
-
-	/*sem_wait(&mutex);
-	read_count--;
-	if (read_count == 0)
-		sem_post(&wrt);
-	sem_post(&mutex);*/
+	pthread_rwlock_rdlock(&rwlock);
+	printf("Reader Thread : Data == %d\n", data);
+	sleep(2);	
+	pthread_rwlock_unlock(&rwlock);
 	return 0;
 }
 
 /*
  * Writer function for writer thread
- *
  */
-
-
 int writer()
 {
-	/*sem_wait(&wrt);
-	data++;*/
-	printf("Writer Thread\n");
+	pthread_rwlock_wrlock(&rwlock);
+	data++;
+	printf("Writer Thread : Data == %d\n", data);
 	sleep(2);
-	/*sem_post(&wrt);*/
+	pthread_rwlock_unlock(&rwlock);
 	return 0;
 }
 
 /*
  * Process thread
- *
  */
-
 void *master_thread(void *ptr)
 {
-
 	int op;
 	printf("This is thread no : %d\n", (int)ptr);
-
 	sem_wait(&mutex);
 	op = remove_item(tp->wq);
-
 	if (op != -1) {
 		/*Call Read operation*/
 		if (op == 1) {
 			reader();
 		}
-		
 		/*Call Write operation*/
 		if (op == 0) {
 			writer();
 		}
 	}
-
-
 	sem_post(&mutex);
-
 }
-
-
-
 
 /*Entry Point*/
 int main()
 {
 	int num, val, i, n_thread;
-
+	
+	/*Initialization of mutex*/
 	sem_init(&mutex, 0, 1);
+	
+	/*Intialization of read-write lock*/
+	pthread_rwlock_init(&rwlock, NULL);
 	
 	/*Create a work to store work item into memory*/
 	struct work_queue_t *wq =  work_queue();
@@ -110,24 +84,33 @@ int main()
 	tp = (struct threadpool *)malloc(sizeof(struct threadpool));
 	tp->wq = wq;
 
+	/*Get the total number of reader writer*/
 	scanf("%d", &num);
 	while (num--) {
 		scanf("%d", &val);
 		add_item(val, wq);
 	}
+
+	/*Display the work items*/
 	display(tp->wq);
 	printf("\n");
 
+	/*Get the number of threads*/
 	printf("Enter Number of Threads\n");
         scanf("%d", &n_thread);
 
+	/*Create threads*/
         for (i = 0; i < n_thread; i++) {
                 pthread_create(&(tp->threads[i]), NULL, master_thread, (void *)i);
         }
 
+	/*Hold till the threads terminate*/
         for (i = 0; i < n_thread; i++) {
                 pthread_join(tp->threads[i], NULL);
         }
+	/*Display the work items*/
+	display(tp->wq);
+	printf("\n");
 	return 0;
 }
 
